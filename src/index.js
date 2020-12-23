192cd5707ab6b9d0619c8a6a5fc941bc30eb1e22
niekh1234 <niekhagen@hotmail.com>
2020-12-23 19:16:34 +0100

    removed unnessary files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import './css/styles.css';
 import './css/rich-text.css';
 import 'alpinejs';
-import './assets/images/ecoscoot_showcase.jpg';
-import './assets/images/goedkoopschap.png';
-import './assets/images/moetikeentest.png';
-import './assets/images/niekh_showcase.png';
 import './js/content';
 
-import work from './static/work.json';
 import skills from './static/skills.json';
