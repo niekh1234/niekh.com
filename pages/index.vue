41b796f6ed861083e22c4be944494b466c6358db
Niek Hagen <niekhagen@hotmail.com>
2021-04-06 18:59:26 +0200

    clean up footer

--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,7 @@
     >
       <img src="~assets/svg/meteors.svg" class="absolute inset-0 w-screen h-screen" />
       <!-- content -->
-      <section class="z-50 flex flex-row max-w-5xl my-auto lg:max-w-4xl h-3/5 lg:h-1/2">
+      <section class="z-50 inline-flex max-w-5xl my-auto lg:max-w-4xl h-3/5 lg:h-1/2">
         <div class="flex flex-col justify-center w-full p-4 mx-0 md:mx-12">
           <h1 class="font-sans text-4xl font-extrabold xs:text-5xl md:text-7xl text-midnight">
             Niek Hagen
@@ -94,7 +94,7 @@
           class="grid w-full h-full grid-cols-1 grid-rows-6 gap-1 mt-8 lg:grid-cols-2 lg:grid-rows-3"
         >
           <template v-if="content.skills.body !== null">
-            <div v-for="skill in content.skills.body" :key="skill.handle" class="flex flex-row p-4">
+            <div v-for="skill in content.skills.body" :key="skill.handle" class="inline-flex p-4">
               <div class="flex justify-center w-1/6 h-full py-3">
                 <svg
                   class="w-12 h-12 p-2 bg-white rounded-lg stroke-current text-orange"
@@ -219,18 +219,19 @@
         </template>
       </section>
     </article>
-    <footer
-      class="flex flex-col items-center w-screen h-auto py-24 bg-blue-darkest text-blue-lightest"
-      id="section_4"
-    >
-      <div class="flex flex-col items-center w-11/12 h-auto sm:w-5/6 md:w-3/4 xl:w-2/3 2xl:w-1/2">
+    <footer class="w-screen h-auto py-16 bg-blue-darkest text-blue-lightest" id="section_4">
+      <div class="max-w-5xl p-8 mx-auto">
         <h3 class="mt-2 text-3xl font-bold text-center sm:text-4xl">Neem contact op</h3>
         <h3 class="w-5/6 mt-2 text-xl font-normal text-center sm:text-2xl">
           Laat een bericht achter of stuur mij een email
         </h3>
-        <form name="contact" class="grid w-full grid-cols-3 grid-rows-2 gap-4 mt-8" netlify>
+        <form
+          name="contact"
+          class="grid w-full grid-flow-row grid-cols-3 gap-2 mt-16 sm:gap-4"
+          netlify
+        >
           <input type="hidden" name="form-name" value="contact" />
-          <label for="name_field" class="col-span-1 mx-auto my-auto font-semibold"> Naam </label>
+          <label for="name_field" class="col-span-3 font-semibold sm:col-span-1">Naam</label>
           <input
             required
             oninvalid="this.setCustomValidity('Dit veld is verplicht')"
@@ -238,9 +239,12 @@
             id="name_field"
             type="text"
             name="name"
-            class="h-8 col-span-2 px-2 transition-colors duration-200 border-2 rounded-lg border-blue-darkest bg-blue-darker focus:outline-none focus:border-blue-dark hover:border-blue-dark"
+            class="h-10 col-span-3 px-2 transition-colors duration-200 border-2 rounded-lg sm:col-span-2 border-blue-darkest bg-blue-darker focus:outline-none focus:border-blue-dark hover:border-blue-dark"
           />
-          <label for="email_field" class="col-span-1 col-start-1 mx-auto my-auto font-semibold">
+          <label
+            for="email_field"
+            class="col-span-3 col-start-1 my-auto font-semibold sm:col-span-1"
+          >
             E-mail
           </label>
           <input
@@ -250,70 +254,78 @@
             id="email_field"
             type="text"
             name="email"
-            class="col-span-2 px-2 transition-colors duration-200 border-2 rounded-lg border-blue-darkest bg-blue-darker focus:outline-none focus:border-blue-dark hover:border-blue-dark"
+            class="h-10 col-span-3 px-2 transition-colors duration-200 border-2 rounded-lg sm:col-span-2 border-blue-darkest bg-blue-darker focus:outline-none focus:border-blue-dark hover:border-blue-dark"
           />
-          <label for="question_field" class="col-span-1 col-start-1 mx-auto my-auto font-semibold">
+          <label
+            for="question_field"
+            class="col-span-3 col-start-1 my-auto font-semibold sm:col-span-1"
+          >
             Bericht
           </label>
           <textarea
             name="question_description"
             id="question_field"
-            class="col-span-2 col-start-2 row-span-2 px-2 transition-colors duration-200 border-2 rounded-lg border-blue-darkest bg-blue-darker focus:outline-none focus:border-blue-dark hover:border-blue-dark"
+            class="h-16 col-span-3 px-2 transition-colors duration-200 border-2 rounded-lg sm:col-start-2 sm:col-span-2 border-blue-darkest bg-blue-darker focus:outline-none focus:border-blue-dark hover:border-blue-dark"
           ></textarea>
           <button
             type="submit"
-            class="col-start-1 row-start-5 px-4 py-2 my-auto ml-auto font-semibold bg-white rounded-lg lg:col-start-3 text-blue-darkest"
+            class="w-full col-span-3 px-4 py-2 my-auto mt-6 ml-auto font-semibold bg-white rounded-lg text-blue-darkest sm:w-auto"
           >
             Versturen
           </button>
         </form>
-      </div>
-      <hr class="w-11/12 h-2 mt-16 border-blue-darker" />
-      <div
-        class="flex flex-row items-center justify-between w-full h-auto px-8 mb-16 md:px-16 lg:mb-4"
-      >
-        <p class="text-grey-lighter text-md sm:text-lg md:text-xl">
-          {{ '© ' + new Date().getFullYear() + ' Niek Hagen' }}
-        </p>
-        <div class="flex flex-row w-auto h-auto">
-          <a href="https://www.github.com/niekh1234" title="GitHub" class="mr-4">
-            <svg viewBox="0 0 512 512" class="w-10 h-10">
-              <path
-                fill="white"
-                d="M256 70.7c-102.6 0-185.9 83.2-185.9 185.9 0 82.1 53.3 151.8 127.1 176.4 9.3 1.7 12.3-4 12.3-8.9V389.4c-51.7 11.3-62.5-21.9-62.5-21.9 -8.4-21.5-20.6-27.2-20.6-27.2 -16.9-11.5 1.3-11.3 1.3-11.3 18.7 1.3 28.5 19.2 28.5 19.2 16.6 28.4 43.5 20.2 54.1 15.4 1.7-12 6.5-20.2 11.8-24.9 -41.3-4.7-84.7-20.6-84.7-91.9 0-20.3 7.3-36.9 19.2-49.9 -1.9-4.7-8.3-23.6 1.8-49.2 0 0 15.6-5 51.1 19.1 14.8-4.1 30.7-6.2 46.5-6.3 15.8 0.1 31.7 2.1 46.6 6.3 35.5-24 51.1-19.1 51.1-19.1 10.1 25.6 3.8 44.5 1.8 49.2 11.9 13 19.1 29.6 19.1 49.9 0 71.4-43.5 87.1-84.9 91.7 6.7 5.8 12.8 17.1 12.8 34.4 0 24.9 0 44.9 0 51 0 4.9 3 10.7 12.4 8.9 73.8-24.6 127-94.3 127-176.4C441.9 153.9 358.6 70.7 256 70.7z"
-              />
-            </svg>
-          </a>
-          <a href="mailto:niekhagen@hotmail.com" title="niekhagen@hotmail.com" class="mr-4">
-            <svg
-              class="w-10 h-10 text-white stroke-current"
-              fill="none"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-              ></path>
-            </svg>
-          </a>
-          <a href="https://github.com/niekh1234/niekh.com" title="Source code">
-            <svg
-              class="w-10 h-10 text-white stroke-current"
-              fill="none"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"
-              ></path>
-            </svg>
-          </a>
+        <hr class="w-11/12 h-2 mt-16 border-blue-darker" />
+        <div class="inline-flex items-center justify-between w-full h-auto mb-2">
+          <p class="text-grey-lighter text-md sm:text-lg md:text-xl">
+            {{ '© ' + new Date().getFullYear() + ' Niek Hagen' }}
+          </p>
+          <div class="inline-flex w-auto h-auto">
+            <a href="https://www.github.com/niekh1234" title="GitHub" class="mr-4">
+              <svg viewBox="0 0 512 512" class="w-10 h-10">
+                <path
+                  fill="white"
+                  d="M256 70.7c-102.6 0-185.9 83.2-185.9 185.9 0 82.1 53.3 151.8 127.1 176.4 9.3 1.7 12.3-4 12.3-8.9V389.4c-51.7 11.3-62.5-21.9-62.5-21.9 -8.4-21.5-20.6-27.2-20.6-27.2 -16.9-11.5 1.3-11.3 1.3-11.3 18.7 1.3 28.5 19.2 28.5 19.2 16.6 28.4 43.5 20.2 54.1 15.4 1.7-12 6.5-20.2 11.8-24.9 -41.3-4.7-84.7-20.6-84.7-91.9 0-20.3 7.3-36.9 19.2-49.9 -1.9-4.7-8.3-23.6 1.8-49.2 0 0 15.6-5 51.1 19.1 14.8-4.1 30.7-6.2 46.5-6.3 15.8 0.1 31.7 2.1 46.6 6.3 35.5-24 51.1-19.1 51.1-19.1 10.1 25.6 3.8 44.5 1.8 49.2 11.9 13 19.1 29.6 19.1 49.9 0 71.4-43.5 87.1-84.9 91.7 6.7 5.8 12.8 17.1 12.8 34.4 0 24.9 0 44.9 0 51 0 4.9 3 10.7 12.4 8.9 73.8-24.6 127-94.3 127-176.4C441.9 153.9 358.6 70.7 256 70.7z"
+                />
+              </svg>
+            </a>
+            <a href="mailto:niekhagen@hotmail.com" title="niekhagen@hotmail.com" class="mr-4">
+              <svg
+                class="w-10 h-10 text-white stroke-current"
+                fill="none"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                ></path>
+              </svg>
+            </a>
+            <a href="https://github.com/niekh1234/niekh.com" title="Source code">
+              <svg
+                class="w-10 h-10 text-white stroke-current"
+                fill="none"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"
+                ></path>
+              </svg>
+            </a>
+          </div>
+        </div>
+        <div class="w-full">
+          <nuxt-link
+            to="/blog"
+            class="mb-16 text-xl font-semibold text-white underline text-blue-lightest hover:no-underline"
+            >Blog</nuxt-link
+          >
         </div>
       </div>
     </footer>
